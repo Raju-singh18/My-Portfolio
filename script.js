b983fa8e9cf1ba30e61b1084f40d96fee9be92d9
Raju-singh18 <rajusingh.mca26@gmail.com>
2025-03-01 21:59:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+ function hamburg(){
+    let navbar = document.querySelector(".dropdown");
+    navbar.style.transform = 'translateY(0px)'; 
+ }
+
+ function cancel(){
+    let navbar = document.querySelector(".dropdown");
+    navbar.style.transform = 'translateY(-500px)'; 
+ }
